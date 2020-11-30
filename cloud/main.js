0b20a20f241e087e0b4a9356338eed031a504dd1
chenyuey <wodechangemail@gmail.com>
2020-11-30 16:53:44 +0800

    添加数据表结构

--- cloud/main.js
+++ cloud/main.js
@@ -4,3 +4,4 @@ require('../utils/wx')
 Parse.Cloud.define('ping', async (req) => {
   return 'pong';
 })
+//获取商户号，实现微信支付功能
--- cloud/model.js
+++ cloud/model.js
@@ -1,22 +1,360 @@
 const SchemaConfig = require('@bigegg/parse-server-schema-config')
-const ToDo = {
-    className: 'ToDo',
+const _User = {
+    className: '_User',
     fields: {
-        name: {
+        openid: {
             type: 'string',
         },
-        password: {
+        avatarUrl: {
             type: 'string',
         },
+        nickName: {
+            type: 'string',
+        },
+        cityId: {
+            type: 'string',
+        },
+        cityName: {
+            type: 'string',
+        },
+        proviceId: {
+            type: 'string',
+        },
+        proviceName: {
+            type: 'string',
+        },
+        speciality: {
+            type: 'string',
+        },
+        university: {
+            type: 'string',
+        },
+        phone: {
+            type: 'string',
+        },
+        realname: {
+            type: 'string',
+        },
+        // role: {
+        //     type: 'string',
+        // },
+        amount: {
+            type: 'number',
+        },
+        score: {
+            type: 'number',
+        }
     },
     CLP: {
         addField: {},
         find: { '*': true },
         count: { '*': true },
         get: { '*': true },
-        create: { 'role:admin': true },
-        update: { 'role:admin': true },
-        delete: { 'role:admin': true },
+        create: { '*': true },
+        update: { 'requiresAuthentication': true },
+        delete: { 'requiresAuthentication': true },
     },
 }
-SchemaConfig.config([ToDo])
+const Subjects = {
+    className: 'Subjects',
+    fields: {
+        subject_name: {
+            type: 'string',
+        },
+        content: {
+            type: 'string',
+        },
+        price: {
+            type: 'number',
+        },
+        level: {
+            type: 'number',
+        },
+        parent_ID: {
+            type: 'string',
+        },
+        has_down_level: {
+            type: 'boolean',
+        }
+    },
+    CLP: {
+        addField: {},
+        find: { '*': true },
+        count: { '*': true },
+        get: { '*': true },
+        create: { 'requiresAuthentication': true },
+        update: { 'requiresAuthentication': true },
+        delete: { 'requiresAuthentication': true },
+    },
+}
+const TestQuestions = {
+    className: 'TestQuestions',
+    fields: {
+        subjects: {
+            type: 'array',
+        },
+        index: {
+            type: 'number',
+        },
+        title: {
+            type: 'string',
+        },
+        type: {
+            type: 'number',
+        },
+        images: {
+            type: 'string',
+        },
+        options: {
+            type: 'array',
+        },
+        isImportant: {
+            type: 'number',
+        },
+        comments: {
+            type: 'string',
+        }
+    },
+    CLP: {
+        addField: {},
+        find: { '*': true },
+        count: { '*': true },
+        get: { '*': true },
+        create: { 'requiresAuthentication': true },
+        update: { 'requiresAuthentication': true },
+        delete: { 'requiresAuthentication': true },
+    },
+}
+const Order = {
+    className: 'Order',
+    fields: {
+        orderNo: {
+            type: 'string',
+        },
+        subjectId: {
+            type: 'string',
+        },
+        subjectName: {
+            type: 'string',
+        },
+        price: {
+            type: 'number',
+        },
+        cash: {
+            type: 'number',
+        },
+        openId: {
+            type: 'string',
+        },
+        state: {
+            type: 'number',
+        },
+        wechatPayOrderId: {
+            type: 'string',
+        }
+    },
+    CLP: {
+        addField: {},
+        find: { '*': true },
+        count: { '*': true },
+        get: { '*': true },
+        create: { 'requiresAuthentication': true },
+        update: { 'requiresAuthentication': true },
+        delete: { 'requiresAuthentication': true },
+    },
+}
+const ActionConfig = {
+    className: 'ActionConfig',
+    fields: {
+        page: {
+            type: 'string',
+        },
+        code: {
+            type: 'string',
+        },
+        action: {
+            type: 'string',
+        },
+        isNeedPay: {
+            type: 'number',
+        },
+        price: {
+            type: 'number',
+        },
+        updatedBy: {
+            type: 'string',
+        }
+    },
+    CLP: {
+        addField: {},
+        find: { '*': true },
+        count: { '*': true },
+        get: { '*': true },
+        create: { 'requiresAuthentication': true },
+        update: { 'requiresAuthentication': true },
+        delete: { 'requiresAuthentication': true },
+    },
+}
+const ErrorHistory = {
+    className: 'ErrorHistory',
+    fields: {
+        questionId: {
+            type: 'string',
+        },
+        title: {
+            type: 'string',
+        },
+        options: {
+            type: 'array',
+        },
+        openid: {
+            type: 'string',
+        }
+    },
+    CLP: {
+        addField: {},
+        find: { '*': true },
+        count: { '*': true },
+        get: { '*': true },
+        create: { 'requiresAuthentication': true },
+        update: { 'requiresAuthentication': true },
+        delete: { 'requiresAuthentication': true },
+    },
+}
+
+const ExamPaper = {
+    className: 'ExamPaper',
+    fields: {
+        test_paper_name: {
+            type: 'string',
+        },
+        subjects: {
+            type: 'array',
+        },
+        options: {
+            type: 'array',
+        },
+        questions: {
+            type: 'array',
+        },
+        time_count: {
+            type: 'number',
+        },
+        score: {
+            type: 'number',
+        },
+        pass_score: {
+            type: 'number',
+        },
+        rang: {
+            type: 'string',
+        },
+        way: {
+            type: 'string',
+        }
+    },
+    CLP: {
+        addField: {},
+        find: { '*': true },
+        count: { '*': true },
+        get: { '*': true },
+        create: { 'requiresAuthentication': true },
+        update: { 'requiresAuthentication': true },
+        delete: { 'requiresAuthentication': true },
+    },
+}
+
+const Message = {
+    className: 'Message',
+    fields: {
+        title: {
+            type: 'string',
+        },
+        content: {
+            type: 'array',
+        }
+    },
+    CLP: {
+        addField: {},
+        find: { '*': true },
+        count: { '*': true },
+        get: { '*': true },
+        create: { 'requiresAuthentication': true },
+        update: { 'requiresAuthentication': true },
+        delete: { 'requiresAuthentication': true },
+    },
+}
+
+const MessageReadHistory = {
+    className: 'MessageReadHistory',
+    fields: {
+        openid: {
+            type: 'string',
+        },
+        MessageIds: {
+            type: 'array',
+        }
+    },
+    CLP: {
+        addField: {},
+        find: { '*': true },
+        count: { '*': true },
+        get: { '*': true },
+        create: { 'requiresAuthentication': true },
+        update: { 'requiresAuthentication': true },
+        delete: { 'requiresAuthentication': true },
+    },
+}
+
+const QuestionHistory = {
+    className: 'QuestionHistory',
+    fields: {
+        openid: {
+            type: 'string',
+        },
+        subjectId: {
+            type: 'array',
+        },
+        questIndex: {
+            type: 'number',
+        },
+        subjectIndex: {
+            type: 'number',
+        },
+        isImportant: {
+            type: 'boolean',
+        }
+    },
+    CLP: {
+        addField: {},
+        find: { '*': true },
+        count: { '*': true },
+        get: { '*': true },
+        create: { 'requiresAuthentication': true },
+        update: { 'requiresAuthentication': true },
+        delete: { 'requiresAuthentication': true },
+    },
+}
+
+const SignInHistory = {
+    className: 'SignInHistory',
+    fields: {
+        openid: {
+            type: 'string',
+        }
+    },
+    CLP: {
+        addField: {},
+        find: { '*': true },
+        count: { '*': true },
+        get: { '*': true },
+        create: { 'requiresAuthentication': true },
+        update: { 'requiresAuthentication': true },
+        delete: { 'requiresAuthentication': true },
+    },
+}
+
+SchemaConfig.config([Subjects,TestQuestions,Order,ActionConfig,
+    ErrorHistory,ExamPaper,Message,MessageReadHistory,QuestionHistory,
+    SignInHistory,_User
+])
